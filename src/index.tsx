3ab85030cc4e9e34b34ebde5f75fe620dd088e64
metalheadpnz <metalheadpnz@gmail.com>
2022-10-10 18:20:34 +0600

    change to HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
 import {App} from './App';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import {store} from "./store/store";
 
@@ -11,9 +10,9 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-    <BrowserRouter>
+    <HashRouter>
         <Provider store={store}>
             <App/>
         </Provider>
-    </BrowserRouter>
+    </HashRouter>
 );
